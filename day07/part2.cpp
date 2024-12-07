325fa504581600e407bc61935e8d65a38c3b9890
MRM <mrolmuo@upv.edu.es>
2024-12-07 20:43:35 +0100

    Day 7

---
+++ day07/part2.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+std::vector<std::int64_t> parse_equation(const std::string& line) {
+    std::istringstream iss(line);
+    std::vector<std::int64_t> result;
+    for (std::int64_t k; iss >> k; iss.ignore(1)) {
+        result.push_back(k);
+    }
+    return result;
+}
+
+std::int64_t digit_concat(std::int64_t a, std::int64_t b) {
+    std::int64_t k = 1;
+    while (k <= b) {
+        k *= 10;
+    }
+    return a*k + b;
+}
+
+bool possible_equation(const std::vector<std::int64_t>& equation) {
+    const std::int64_t target = equation.at(0);
+    std::vector<std::int64_t> current = {equation.at(1)};
+    for (std::size_t i = 2; i < equation.size(); ++i) {
+        const std::int64_t k = equation.at(i);
+        std::vector<std::int64_t> next;
+        for (std::int64_t e : current) {
+            const std::array<std::int64_t, 3> values = {{
+                e + k,
+                e * k,
+                digit_concat(e, k),
+            }};
+            for (std::int64_t v : values) {
+                if (v <= target) {
+                    next.push_back(v);
+                }
+            }
+        }
+        current = std::move(next);
+    }
+    return std::find(current.begin(), current.end(), target) != current.end();
+}
+
+int main() {
+    std::int64_t result = 0;
+    for (std::string line; std::getline(std::cin, line) && line.size() > 0;) {
+        auto equation = parse_equation(line);
+        if (possible_equation(equation)) {
+            result += equation.at(0);
+        }
+    }
+    std::cout << result << '\n';
+}
